795bfe350d7a96cd72e1167ec3e88d3419f5cb12
Giovanni Carvalheira <giovannicmp@hotmail.com.br>
2020-03-15 17:02:42 -0300

    Versão 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//Permite a utilização do HttpClient.
 import { HttpClientModule } from '@angular/common/http';
-//Auxilia a criar o formulário.
 import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
--- src/app/models/funcionario.ts
+++ src/app/models/funcionario.ts
@@ -5,3 +5,10 @@ export interface Funcionario {
     salario: number;
     email: string;
 }
+export interface Funcionario2 {
+    id: number;
+    nome: string;
+    cargo: string;
+    salario: number;
+    email: string;
+}
